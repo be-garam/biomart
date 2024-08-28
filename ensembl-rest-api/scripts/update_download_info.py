cd7eff45a261060e098a5e59a3c9e6d326157c2b
be-garam <be.garam.dev@gmail.com>
2024-08-28 13:59:19 +0900

    FIX: download_info's links error

--- ensembl-rest-api/scripts/update_download_info.py
+++ ensembl-rest-api/scripts/update_download_info.py
@@ -27,8 +27,9 @@ def generate_download_info():
         
         for species in species_info:
             species_name = species['name']
-            assembly_info = species.get('assembly_info', {})
-            assembly_name = assembly_info.get('assembly_name')
+            assembly_name = species['assembly']
+            # assembly_info = species[.get('assembly_info', {})]
+            # assembly_name = assembly_info.get('assembly_name')
             release = species['release']
 
             if not assembly_name:
@@ -44,16 +45,19 @@ def generate_download_info():
 
             download_info[species_name] = {"release": release, "assembly": assembly_name, "files": {}}
 
-            for seq_type in ['dna', 'dna_sm', 'dna_rm']:
+            # for seq_type in ['dna', 'dna_sm', 'dna_rm']:
+            for seq_type in ['dna']:
                 for id_type in ['toplevel', 'primary_assembly']:
-                    filename = f"{species_name}.{assembly_name}.{seq_type}.{id_type}.fa.gz"
+                    f_species_name = species_name[0].upper() + species_name[1:]
+                    filename = f"{f_species_name}.{assembly_name}.{seq_type}.{id_type}.fa.gz"
                     if check_file_exists(ftp, filename):
-                        url = f"ftp://{FTP_SERVER}{ftp_path}{filename}"
+                        url = f"https://{FTP_SERVER}{ftp_path}{filename}"
                         download_info[species_name]["files"][f"{seq_type}_{id_type}"] = url
                         print(f"Added {seq_type}_{id_type} for {species_name}")
 
             if not download_info[species_name]["files"]:
                 print(f"Warning: No files found for {species_name}")
+                print(f"url: {url}")
                 del download_info[species_name]
 
     return download_info
